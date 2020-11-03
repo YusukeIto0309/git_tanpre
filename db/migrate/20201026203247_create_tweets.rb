a8bfb0f7c605653109591e7a4e589d65c6c5d4b6
EC2 Default User <ec2-user@ip-172-31-13-200.ap-northeast-1.compute.internal>
2020-11-03 21:14:08 +0000

    nameカラムとtitleカラムを追加

--- db/migrate/20201026203247_create_tweets.rb
+++ db/migrate/20201026203247_create_tweets.rb
@@ -2,9 +2,9 @@ class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
 
-
+      t.string               :name
       t.string               :title
-      t.binary               :image
+    
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_203247) do
+ActiveRecord::Schema.define(version: 2020_11_03_211047) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.string "title"
-    t.binary "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
