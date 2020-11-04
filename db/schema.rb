95a9eb3a3a640eb739d93d1ea26eb05da8b7d117
EC2 Default User <ec2-user@ip-172-31-13-200.ap-northeast-1.compute.internal>
2020-11-04 20:24:23 +0000

    データベースにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_211047) do
+ActiveRecord::Schema.define(version: 2020_11_04_202006) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
